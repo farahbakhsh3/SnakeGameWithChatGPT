6993d57d163d1cd64f9432173a2810923597ce6d
farahbakhsh3 <farahbakhsh3@gmail.com>
2023-04-06 18:56:22 +0330

    Add project files.

---
+++ snake/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace SnakeGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(0, 0);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(35, 13);
+            this.lblScore.TabIndex = 0;
+            this.lblScore.Text = "label1";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblScore);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ snake/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SnakeGame
+{
+    public partial class Form1 : Form
+    {
+        private static int snakeBlock = 20;
+        private static int snakeSpeed = 20;
+        private int screenWidth = snakeBlock * 20;
+        private int screenHeight = snakeBlock * 15;
+        private int foodX, foodY;
+        private int score = 0;
+
+        private List<Point> snakeList = new List<Point>();
+        private string direction = "right";
+
+        public Form1()
+        {
+            InitializeComponent();
+            StartGame();
+        }
+
+        private void StartGame()
+        {
+            ResetGame();
+            timer1.Interval = 100;
+            timer1.Start();
+        }
+
+        private void ResetGame()
+        {
+            snakeList.Clear();
+            snakeList.Add(new Point(snakeBlock*3, snakeBlock));
+            snakeList.Add(new Point(snakeBlock*2, snakeBlock));
+            snakeList.Add(new Point(snakeBlock*1, snakeBlock));
+
+            direction = "right";
+
+            GenerateFood();
+            score = 0;
+
+            lblScore.Text = "Score: 0";
+            timer1.Start();
+        }
+
+        private void GenerateFood()
+        {
+            Random random = new Random();
+            foodX = random.Next(0, screenWidth / snakeBlock) * snakeBlock;
+            foodY = random.Next(0, screenHeight / snakeBlock) * snakeBlock;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right && direction != "left")
+            {
+                direction = "right";
+            }
+            else if (e.KeyCode == Keys.Left && direction != "right")
+            {
+                direction = "left";
+            }
+            else if (e.KeyCode == Keys.Up && direction != "down")
+            {
+                direction = "up";
+            }
+            else if (e.KeyCode == Keys.Down && direction != "up")
+            {
+                direction = "down";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            MoveSnake();
+            CheckCollision();
+            this.Invalidate();
+        }
+
+        private void MoveSnake()
+        {
+            Point head = snakeList.First();
+
+            if (direction == "right")
+            {
+                head.X += snakeSpeed;
+            }
+            else if (direction == "left")
+            {
+                head.X -= snakeSpeed;
+            }
+            else if (direction == "up")
+            {
+                head.Y -= snakeSpeed;
+            }
+            else if (direction == "down")
+            {
+                head.Y += snakeSpeed;
+            }
+
+            if (head.X >= screenWidth)
+            {
+                head.X = 0;
+            }
+            else if (head.X < 0)
+            {
+                head.X = screenWidth - snakeBlock;
+            }
+
+            if (head.Y >= screenHeight)
+            {
+                head.Y = 0;
+            }
+            else if (head.Y < 0)
+            {
+                head.Y = screenHeight - snakeBlock;
+            }
+
+            snakeList.Insert(0, head);
+
+            if (head.X == foodX && head.Y == foodY)
+            {
+                score += 10;
+                lblScore.Text = "Score: " + score;
+                timer1.Interval = Math.Max(timer1.Interval-2, 35);
+
+                GenerateFood();
+            }
+            else
+            {
+                snakeList.RemoveAt(snakeList.Count - 1);
+            }
+        }
+
+        private void CheckCollision()
+        {
+            Point head = snakeList.First();
+            for (int i = 1; i < snakeList.Count; i++)
+            {
+                if (snakeList[i].X == head.X && snakeList[i].Y == head.Y)
+                {
+                    timer1.Stop();
+                    MessageBox.Show("Game Over!");
+                    ResetGame();
+                    break;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics canvas = e.Graphics;
+
+            Pen snakePen = new Pen(Color.Blue, 3);
+            Brush snakeBrush = Brushes.Blue;
+            Brush foodBrush = Brushes.Red;
+
+            foreach (Point point in snakeList)
+            {
+                //canvas.DrawRectangle(snakePen, point.X, point.Y, snakeBlock, snakeBlock);
+                canvas.FillRectangle(snakeBrush, point.X, point.Y, snakeBlock, snakeBlock);
+            }
+
+            canvas.FillRectangle(foodBrush, foodX, foodY, snakeBlock, snakeBlock);
+        }
+    }
+}
